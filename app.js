b7a6e5871f8dd4e6f38e5edfa3ea93f3ad5a860b
sachinrajc <sachinrajc7@gmail.com>
2024-06-12 20:23:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const myFullName = 'Sachin Raj Charupadikkal';
 const myStudentNumber = 1147316;
 const TemplateLiterals = `${myFullName} - ${myStudentNumber}`
-
-console.log(myFullName);
-console.log(myStudentNumber);
-
 console.log(TemplateLiterals);
 
 const headerContent = document.querySelector(`h1`);
